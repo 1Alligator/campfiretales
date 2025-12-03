35e35f535564a9ae439439fd8419cd2f34713956
1Alligator <101168398+1Alligator@users.noreply.github.com>
2025-12-02 22:15:41 -0600

    Created Project

---
+++ src/main/java/net/shnep/campfiretales/CampfireTalesClient.java
@@ -0,0 +1,11 @@
+package net.shnep.campfiretales;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CampfireTalesClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/shnep/campfiretales/CampfireTalesDataGenerator.java
@@ -0,0 +1,11 @@
+package net.shnep.campfiretales;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CampfireTalesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
